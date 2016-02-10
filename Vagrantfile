d729000340413338641157e984a454433f0c9ca5
Liam Haworth <liam.haworth@bluereef.com.au>
2016-02-10 13:40:43 +1100

    Did a masive clean up on the build code

--- Vagrantfile
+++ Vagrantfile
@@ -4,30 +4,13 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty32"
 
-  config.vm.synced_folder "cd_root", "/data/cd_root"
-  config.vm.synced_folder "rd_files", "/data/rd_files"
+  config.vm.synced_folder "src", "/data/src"
   config.vm.synced_folder "output", "/data/output"
-  config.vm.synced_folder "defines", "/data/defines"
 
   config.vm.provision "shell", inline: <<-SHELL
     sudo locale-gen en_AU.UTF-8
     echo "nameserver 8.8.8.8" > /etc/resolv.conf 
-
-    echo "###### INSTALLING 3RD PARTY TOOLS"
     sudo apt-get -qq update
     sudo apt-get -qq install unzip genisoimage genometools zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libyajl2 build-essential -y
-    
-    echo "###### DOWNLOADING SYSLINUX"
-    mkdir -p /tmp/syslinux
-    cd /tmp/syslinux
-    wget -q https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip 
-    unzip -q syslinux-6.03.zip
-    
-    echo "###### BUILDING ISO FILESYSTEM AND ADDING ISOLINUX"
-    mkdir -p /data/cd_root/{isolinux,images}
-    cp bios/core/isolinux.bin /data/cd_root/isolinux/
-    cp bios/com32/elflink/ldlinux/ldlinux.c32 /data/cd_root/isolinux/
-
-    echo "###### ENVIRONMENT READY"
   SHELL
 end
--- build.sh
+++ build.sh
@@ -4,69 +4,101 @@ function logInfo {
 	echo -e "\e[92m## ${1}\e[0m"
 }
 
-function extractInitrd {
-	logInfo "Extracting InitRD from build system"
-	vagrant ssh -c "cp /boot/initrd.img-\$(uname -r) /data/rd_files/"
-	vagrant ssh -c "cd /data/rd_files; zcat initrd.img-\$(uname -r) | cpio -i; rm initrd.img-\$(uname -r)"
+function runCommand {
+	output=`vagrant ssh -c "${1}"`
+
+	if [ $? -ne 0 ]; then
+		echo "ERROR: Command execution failed!" > /dev/stderr
+		echo -e "PURPOSE:\t${2}" > /dev/stderr
+		echo -e "COMMAND:\t${1}" > /dev/stderr
+		echo -e "OUTPUT:\n\n${output}" > /dev/stderr
+		exit 1
+	fi
+}
+
+function cleanStart {
+	logInfo "Building clean environment"
+	vagrant destroy -f
+	vagrant up
+}
+
+function prepareSource {
+	logInfo "Preparing RAM disk source"
+	runCommand "cp -r /data/src /tmp/src" "Copy source code to temp directory before build"
+	runCommand "cd /tmp/src; sudo mknod dev/console c 5 1" "Make device node for console"
+	runCommand "cd /tmp/src; sudo mknod dev/ram0 b 1 1" "Make device node for ram0"
+	runCommand "cd /tmp/src; sudo mknod dev/null c 1 3" "Make device node for Null"
+	runCommand "cd /tmp/src; sudo mknod dev/tty1 c 4 1" "Make device node for TTY1"
+	runCommand "cd /tmp/src; sudo mknod dev/tty2 c 4 2" "Make device node for TTY2"
+	runCommand "cd /tmp/src; ln -s bin sbin" "Symlink bin to sbin"
+	runCommand "sudo chown vagrant:vagrant -R /tmp/src" "Change ownership on source folder to vagrant user"
 }
 
 function compileRuby {
 	logInfo "Compilling Ruby 2.3 for RAM Disk"
-	vagrant ssh -c "cd /tmp; wget -q https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.0.tar.gz; tar xzf ruby-2.3.0.tar.gz;"
-	vagrant ssh -c "cd /tmp/ruby-2.3.0/; ./configure --prefix=/data/rd_files/usr/local"
-	vagrant ssh -c "cd /tmp/ruby-2.3.0/; make; make install; cp -r ./include /data/rd_files/lib/ruby/include"
+	runCommand "cd /tmp; wget -q https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.0.tar.gz; tar xzf ruby-2.3.0.tar.gz;" "Download and extract Ruby source code"
+	runCommand "cd /tmp/ruby-2.3.0/; ./configure --prefix=/tmp/src/usr/local" "Configure Ruby source code with correct build location"
+	runCommand "cd /tmp/ruby-2.3.0/; make; make install; cp -r ./include /tmp/src/usr/local/lib/ruby/include" "Compile Ruby source code"
 }
 
 function installGems {
-	logInfo "Installing Ohai gem"
-	vagrant ssh -c "/data/rd_files/bin/gem install ohai"
+	logInfo "Installing gems"
+	runCommand "/tmp/src/usr/local/bin/gem install ohai" "Install Ohai gem"
 }
 
-function cleanStart {
-	logInfo "## Building clean environment"
-	vagrant destroy -f
-	vagrant up
+function buildRamDisk {
+	logInfo "Building ram disk"
+	runCommand "cd /tmp/src; find . | cpio -o -H newc | gzip > ../initrd.img" "Compress and create initrd"
+	runCommand "cp /tmp/initrd.img /data/output/" "Copy initrd to output directory"
 }
 
 function showHelp() {
 	echo "Usage: ./build.sh [COMMAND]"
 	echo
-	echo -e "\textract-initrd - Extracts the initrd img from the Vagrant host"
-	echo -e "\tcompile-ruby - Downloads and compiles Ruby into the rd"
 	echo -e "\tclean-start - Destroys and builds a new Vagrant VM"
+	echo -e "\tprepare-source - Copys source code to temp directory and adds device nodes"
+	echo -e "\tcompile-ruby - Downloads and compiles Ruby"
 	echo -e "\tinstall-gems - Install all required ruby gems"
+	echo -e "\tbuild-ram-disk - Builds the initrd.img file from the source code"
 	echo -e "\tall - Runs all the above tasks in order"
 	echo
 	exit 0
 }
 
 case $1 in
-	"extract-initrd")
-		extractInitrd
+	"clean-start")
+		cleanStart
+		;;
+
+	"prepare-source")
+		prepareSource
 		;;
 
 	"compile-ruby")
 		compileRuby
 		;;
 
-	"clean-start")
-		cleanStart
-		;;
-
 	"install-gems")
 		installGems
 		;;
 
+	"build-ram-disk")
+		buildRamDisk
+		;;
+
 	"all")
 		cleanStart
-		extractInitrd
+		prepareSource
 		compileRuby
 		installGems
+		buildRamDisk
 		;;
 
 	*)
+		echo "ERROR: Unsupported comannd $1\n"
 		showHelp
 		;;
+
 	"help")
 		showHelp
 		;;
